66b477989b252de6b2da2bd7c4dca2d9605247ca
wk-j <somnuk.wk@gmail.com>
2018-08-10 11:01:15 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,60 @@
+## Dot Space
+
+Display size of dot folders in home directory
+
+## Support
+
+- [x] macOS
+- [ ] Linux
+- [ ] Windows
+
+## Installation
+
+```bash
+dotnet tool install -g wk.DotSpace
+```
+
+## Usage
+
+```bash
+wk-dot-space
+
+996K	.dbeaver4
+2.4G	.stack
+4.0K	.eclipse
+ 72K	.config
+ 20K	.rest-client
+ 12K	.docker
+9.9M	.node-gyp
+4.0K	.lldb
+ 76K	.tooling
+ 32K	.subversion
+  0B	.omnisharp
+217M	.sonar
+ 15M	.templateengine
+1.1G	.local
+  0B	.mysqlsh
+3.0G	.nuget
+141M	.electron
+ 20K	.oracle_jre_usage
+4.0K	.VSCodeVim
+ 24K	.bash_sessions
+  0B	.lein
+196K	.AlfrescoExplorer
+2.8M	.dbeaver-drivers
+4.0K	.ssh
+611M	.dotnet
+ 34M	.Trash
+276K	.tweets
+716K	.vim-anywhere
+  0B	.vscode-insiders
+583M	.npm
+4.0K	.mono
+1.9G	.vscode
+786M	.m2
+8.0K	.aspnet
+  0B	.cache
+5.1M	.bookmarks
+4.0K	.httpie
+ 11G	total
+```
---
+++ src/DotSpace/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace DotSpace {
+    class Program {
+
+        static void StartProcess(string command, string args, string dir) {
+            var ps = new Process {
+                StartInfo = new ProcessStartInfo {
+                    FileName = command,
+                    Arguments = args,
+                    UseShellExecute = false,
+                    WorkingDirectory = dir
+                }
+            };
+            ps.Start();
+            ps.WaitForExit();
+        }
+
+        static void Main(string[] args) {
+            var home = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+            var dirInfo = new DirectoryInfo(home).GetDirectories(".*");
+            var joined = dirInfo.Select(x => x.Name).Aggregate((a, b) => a + " " + b);
+            StartProcess("du", "-hcs " + joined, home);
+        }
+    }
+}
